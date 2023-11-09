550b5c209eb966b01f33faccff92903f094c447a
naveenkumar-s563104 <143102810+naveenkumar-s563104@users.noreply.github.com>
2023-11-08 21:25:35 -0600

    assignment successfully

--- controllers/dress.js
+++ controllers/dress.js
@@ -14,10 +14,30 @@ exports.dress_list = async function(req, res) {
 exports.dress_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Dress detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
-exports.dress_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Dress create POST');
+
+
+// Handle Earbud create on POST.
+exports.dress_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Dress();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"dress_brand": "Jockey", "dress_size": "XXL", "dress_cost": 556} 
+    document.dress_brand = req.body.dress_brand;
+    document.dress_size = req.body.dress_size;
+    document.dress_cost = req.body.dress_cost;
+    try{
+        let result = await document.save();
+        res.send(result);
+    } catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    } 
 };
+
+
+
 // Handle Costume delete form on DELETE.
 exports.dress_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Dress delete DELETE ' + req.params.id);
@@ -26,3 +46,16 @@ res.send('NOT IMPLEMENTED: Dress delete DELETE ' + req.params.id);
 exports.dress_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Dress update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.dress_view_all_Page = async function(req, res) {
+    try{
+    theDress = await Dress.find();
+    res.render('dress', { title: 'Dress Search Results', results: theDress });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
--- routes/dress.js
+++ routes/dress.js
@@ -1,9 +1,6 @@
 var express = require('express');
+const dress_controlers= require('../controllers/dress');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('dress', { title: 'Search Result Dress' });
-});
-
+/* GET costumes */
+router.get('/', dress_controlers.dress_view_all_Page );
 module.exports = router;
