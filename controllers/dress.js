5264ccd20b988081ef376e89da1d87ed6fecffb6
naveenkumar-s563104 <143102810+naveenkumar-s563104@users.noreply.github.com>
2023-11-17 10:50:09 -0600

    updated

--- controllers/dress.js
+++ controllers/dress.js
@@ -11,12 +11,19 @@ exports.dress_list = async function(req, res) {
     }
    };
 // for a specific Costume.
-exports.dress_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Dress detail: ' + req.params.id);
+exports.dress_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Dress.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 
 
-// Handle Earbud create on POST.
+// Handle dress create on POST.
 exports.dress_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Dress();
@@ -43,8 +50,24 @@ exports.dress_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Dress delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.dress_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Dress update PUT' + req.params.id);
+exports.dress_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+     try {
+     let toUpdate = await Dress.findById( req.params.id)
+     // Do updates of properties
+     if(req.body.dress_type)
+     toUpdate.dress_type = req.body.dress_type;
+     if(req.body.cost) toUpdate.cost = req.body.cost;
+     if(req.body.size) toUpdate.size = req.body.size;
+     let result = await toUpdate.save();
+     console.log("Sucess " + result)
+     res.send(result)
+     } catch (err) {
+     res.status(500)
+     res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+     }
 };
 
 // VIEWS
@@ -57,5 +80,70 @@ exports.dress_view_all_Page = async function(req, res) {
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
+    }
+   };
+
+// Handle Dress delete form on DELETE.
+exports.dress_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Dress.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.dress_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+        result = await Dress.findById( req.query.id)
+        res.render('dressdetail',{ title: 'Dress Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+ };
+ // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.dress_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('dresscreate', { title: 'Dress Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for updating a dress
+// query provides the id
+exports.dress_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Dress.findById(req.query.id)
+    res.render('dressupdate', { title: 'Dress Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle a delete one view with id from query
+exports.dress_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Dress.findById(req.query.id)
+    res.render('dressdelete', { title: 'Dress Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
    };
--- routes/dress.js
+++ routes/dress.js
@@ -1,6 +1,18 @@
 var express = require('express');
-const dress_controlers= require('../controllers/dress');
+const dress_controller= require('../controllers/dress');
 var router = express.Router();
 /* GET costumes */
-router.get('/', dress_controlers.dress_view_all_Page );
+router.get('/', dress_controller.dress_view_all_Page );
+
+/* GET detail costume page */
+router.get('/detail', dress_controller.dress_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', dress_controller.dress_create_Page);
+/* GET delete costume page */
+router.get('/delete', dress_controller.dress_delete_Page);
+
+
+/* GET create update page */
+router.get('/update', dress_controller.dress_update_Page);
 module.exports = router;
